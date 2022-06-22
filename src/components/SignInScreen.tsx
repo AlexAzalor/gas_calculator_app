8268739c568a5aa5a4267aeef0c98b3bfc9369a8
AlexAzalor <xy55xy.azalor@gmail.com>
2022-06-22 21:11:56 +0300

    google auth

--- src/components/SignInScreen.tsx
+++ src/components/SignInScreen.tsx
@@ -1,8 +1,8 @@
 import { useNavigation } from '@react-navigation/native';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { FieldValues, useForm } from 'react-hook-form';
-import { Image, StyleSheet, Text, View, useWindowDimensions } from 'react-native';
+import { Image, StyleSheet, Text, View, useWindowDimensions, Button } from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
 import Logo from '../../assets/square100.png';
 import { instanceLogin } from '../api/auth/login';
@@ -10,6 +10,10 @@ import { CustomButton } from './CustomButton';
 import { CustomInput } from './CustomInput';
 import { SocialButtons } from './SocialButtons';
 import expo from '../../app.json'
+import * as Google from 'expo-auth-session/providers/google';
+import * as WebBrowser from 'expo-web-browser';
+
+WebBrowser.maybeCompleteAuthSession();
 
 type RootStackParamList = {
   HomeScreen: undefined;
@@ -23,6 +27,42 @@ export const SignInScreen = () => {
   const { height } = useWindowDimensions();
   const navigation = useNavigation<ProfileScreenNavigationProp>();
   const { control, handleSubmit } = useForm();
+
+  const [accessToken, setAccessToken] = useState();
+  const [userInfo, setUserInfo] = useState();
+
+  const [request, response, promptAsync] = Google.useAuthRequest({
+    androidClientId: "339566261787-26jrgtic0lh0jlqh2md28hr70fr4c1f9.apps.googleusercontent.com",
+    expoClientId: "339566261787-jp7vot14696cfuiqhjjsmdna93jo12gf.apps.googleusercontent.com"
+  })
+
+  useEffect(() => {
+    if (response?.type === 'success') {
+      setAccessToken(response.authentication.accessToken);
+    }
+  }, [response]);
+
+  async function getUserData() {
+    let userInfoResponse = await fetch('https://www.googleapis.com/userinfo/v2/me', {
+      headers: { Authorization: `Bearer ${accessToken}` }
+    })
+
+    userInfoResponse.json().then(data => {
+      setUserInfo(data);
+    })
+  }
+
+  function showUserInfo() {
+    if (userInfo) {
+      return (
+        <View style={styles.userInfo}>
+          <Image source={{ uri: userInfo.picture }} style={styles.profilePic} />
+          <Text>Welcome {userInfo.name} </Text>
+          <Text>{userInfo.email} </Text>
+        </View>
+      )
+    }
+  }
 
   const getRegisteredUser = async (data: FieldValues) => {
     const response = await instanceLogin(data.username, data.password).get('/api/login');
@@ -57,6 +97,13 @@ export const SignInScreen = () => {
 
         <Text>App version: {expo.expo.version}</Text>
         <Text>Android app version: {expo.expo.android.versionCode}</Text>
+
+        {showUserInfo()}
+
+        <Button
+          title={accessToken ? 'Get User Data' : 'Login'}
+          onPress={accessToken ? getUserData : () => { promptAsync({ showInRecents: true }) }}
+        />
 
         <CustomInput
           name="username"
